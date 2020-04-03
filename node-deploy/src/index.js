2f64baedc706af2eaf67eb8a8dd394714776aa62
ajkatom <alonadelson@alons-MacBook-Pro.local>
2020-04-03 17:52:43 -0400

    commit

--- node-deploy/src/index.js
+++ node-deploy/src/index.js
@@ -1,1 +1,7 @@
 #!usr/bin/env node
+const AWS = require('aws-sdk');
+const child_porcess = require('child_porcess');
+const { format } = require('date-fns');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
